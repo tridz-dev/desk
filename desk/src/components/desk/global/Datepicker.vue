6e73c9c4d78ab23cf9f135c1ed734eefffdbdf5a
esafwan <esafwan@gmail.com>
2022-12-29 09:24:46 +0000

    Created date picker a component & added logic for default date

---
+++ desk/src/components/desk/global/Datepicker.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<Datepicker
+			range
+			v-model="selectedDates"
+			@change="onDateChange"
+			:enable-time-picker="false"
+			format="MMM dd"
+		/>
+	</div>
+</template>
+
+<script>
+import Datepicker from "@vuepic/vue-datepicker"
+
+export default {
+    components: {
+        Datepicker,
+    },
+    computed: {
+        startDate() {
+            return new Date(new Date().setDate(new Date().getDate() - 7));
+        },
+        endDate() {
+            return new Date();
+        },
+        selectedDates() {
+            return [this.startDate, this.endDate]
+        }
+    },
+    methods: {
+        onDateChange() {
+            this.$emit("date-change", this.selectedDates)
+			console.log("here")
+        },
+    },
+}
+</script>
